6c7d71dc027af33325da5c5618c705c4433521f9
RMMusic <r.m.alien.ua@gmail.com>
2016-03-16 01:44:29 +0200

    qqwerty

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,21 @@
  */
 function menu(){
     $('.registerForm').on('click', function(){
+        $(this).parent().toggleClass('active');
         $('#registerForm').templateLoader({
             caching: false,
-            templateRenderCallback: function(template){
+            templateRenderCallback: function(){
+                yo();
+            }
+        });
+    });
+    $('.ruslan').on('click', function(){
+        $(this).parent().toggleClass('active');
+        $('#ruslanHTML').templateLoader({
+            caching: false,
+            templateRenderCallback: function(){
                 yo();
             }
         });
     })
 }
-
-
